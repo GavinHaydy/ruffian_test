bdc30b4af76cdecd20e0a73b8f1d2e8f32f9bdf8
bugpz <13558622779@163.com>
2022-02-23 14:11:21 +0800

    feat：安装文件

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", 'rb') as f:
+    long_description = f.read()
+
+setup(name='ruffianTest',  # 你包的名称
+      version='1.0.0',  # 版本号
+      description='test',  # 描述
+      long_description=long_description,  # 长描述
+      long_description_content_type='text/markdown',
+      author='The-Ruffian',
+      author_email='',
+      url='https://github.com/the-ruffian/ruffian_test',
+      download_url='https://github.com/the-ruffian/ruffian_test',
+      install_requires=[],  # 依赖第三方库
+      license='MIT License',
+      keywords=['bug_pz', 'the-ruffian', 'ruffianTest', 'ruffian', 'test'],
+      packages=find_packages(),
+      platforms=['all'],  # 平台
+      classifiers=[
+          'Intended Audience :: Testers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python :: 3.0',
+          'Topic :: Software Test :: Libraries'
+      ],
+      )
