be231a75da2efa7f6c3e54b59f1c450363e3ce73
bugpz <13558622779@163.com>
2022-03-01 16:05:23 +0800

    feat：新增按范围取值方法

--- ruffianTest/common/document/csv.py
+++ ruffianTest/common/document/csv.py
@@ -1,4 +1,7 @@
 import csv
+import logging
+
+logging.basicConfig(level=logging.ERROR)
 
 
 class OperationCSV:
@@ -16,10 +19,45 @@ class OperationCSV:
             reader = csv.reader(file)
             rows = [row for row in reader]
             return rows[row_num][col_num]
+
+    def get_values(self, row_start, row_end, col_start, cor_end):
         """
-            s = OperationCSV('test.csv')
-            print(s.get_value(0, 1))
+
+        :param row_start: 开始行
+        :param row_end: 结束行
+        :param col_start: 开始列
+        :param cor_end: 结束列
+        :return: 查询结果
         """
+        # 物理数改为索引
+        row_start = row_start - 1
+        row_end = row_end - 1
+        col_start = col_start - 1
+        cor_end = cor_end - 1
+        with open(self.path, 'r') as file:
+            reader = csv.reader(file)
+            results = []
+            for index, info in enumerate(reader):   # 获取索引和对应数据
+                if row_start < row_end and col_start < cor_end:
+                    if row_start <= index <= row_end:
+                        results.append(info[col_start:cor_end + 1])
+                elif row_start < row_end and col_start == cor_end:
+                    if row_start <= index <= row_end:
+                        results.append(info[col_start:col_start + 1])
+                elif row_start == row_end and col_start < cor_end:
+                    if row_start == index:
+                        results.append(info[col_start: cor_end + 1])
+                elif row_start == row_end and col_start == cor_end:
+                    if row_start == index:
+                        results.append(self.get_value(row_start, col_start))
+                else:
+                    if row_start > row_end and col_start > cor_end:
+                        return logging.error('row_start>row_end and col_start > cor_end')
+                    elif col_start > cor_end:
+                        return logging.error('col_start>cor_end')
+                    else:
+                        return logging.error('row_start > row_end')
+            return results
 
     def write_value(self, values, headers=None):
         """
@@ -41,4 +79,3 @@ class OperationCSV:
             s = OperationCSV('{path}.csv')
             s.write_value([('1', '18', '测试'), ('2', '20', '没bug')], {'id', 'age', 'name'})
         """
-
