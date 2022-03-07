258bb8debb79b7f92ee6ff67208a8f68a77ac33b
bugpz <13558622779@163.com>
2022-03-07 10:39:02 +0800

    feat：新增写入类

--- ruffianTest/common/document/operation_excel.py
+++ ruffianTest/common/document/operation_excel.py
@@ -1,9 +1,16 @@
 import xlrd
+import xlwt
+from xlwt import Pattern, Style, XFStyle
 
 
 class OperationExcel:
-    def __init__(self, path, sheet_name):
-        self.workbook = xlrd.open_workbook(path)
+    def __init__(self, file_name, sheet_name):
+        """
+
+        :param file_name: 路径+文件名
+        :param sheet_name: 工作表名
+        """
+        self.workbook = xlrd.open_workbook(file_name)
         self.sheet = self.workbook.sheet_by_name(sheet_name)
 
     def get_value(self, row, col):
@@ -26,6 +33,66 @@ class OperationExcel:
         return results
 
 
-if __name__ == '__main__':
-    s = OperationExcel('/home/bugpz/文档/快快/测试用例快快V2.6.1.xlsx','快快V2.6.1').get_values(4,5,1,0)
-    print(s)
+class TestExcel:
+    def __init__(self, file_name, sheet_name):
+        """
+
+        :param file_name: 文件地址+文件名
+        :param sheet_name: 工作表名
+        """
+        self.file_name = file_name
+        self.book = xlwt.Workbook()  # 新建工作簿
+        self.sheet = self.book.add_sheet(sheet_name)  # 新建sheet
+
+    @staticmethod
+    def set_cell_style(color, font_name='宋体', font_bold=False, font_italic=False, font_underline=False):
+        # 颜色处理
+        new_pattern = Pattern()
+        new_pattern.pattern = Pattern.SOLID_PATTERN
+        new_pattern.pattern_fore_colour = Style.colour_map[color]
+        new_style = XFStyle()
+        new_style.pattern = new_pattern
+        # 字体处理
+        font = xlwt.Font()
+        font.name = font_name  # 字体名称
+        font.bold = font_bold  # 加粗
+        font.italic = font_italic  # 斜体
+        font.underline = font_underline  # 下划线
+        new_style.font = font
+        return new_style
+
+    def set_value(self, row, col, body, color='white', font_name='宋体', font_bold=False, font_italic=False,
+                  font_underline=False):
+        style = self.set_cell_style(color, font_name, font_bold, font_italic, font_underline)
+        self.sheet.write(row - 1, col - 1, body, style)
+        self.book.save(self.file_name)
+
+    def set_values(self, row, col, values, headers='', color='red',
+                   font_name='宋体', font_bold=False, font_italic=False, font_underline=False):
+        """
+
+        :param row:
+        :param col:
+        :param values: [(),()...]
+        :param headers: {}
+        :param color:
+        :param font_name: 字体名称
+        :param font_bold: 加粗
+        :param font_italic: 斜体
+        :param font_underline: 下划线
+        :return:
+        """
+        row, col = row - 1, col - 1
+        style = self.set_cell_style(color, font_name, font_bold, font_italic, font_underline)
+        if headers == '':
+            for index, item in enumerate(values):
+                for index_col, item_col in enumerate(item):
+                    self.sheet.write(row + index, col + index_col, item_col, style)
+                    self.book.save(self.file_name)
+        else:
+            for index_h, item_h in enumerate(headers):
+                self.sheet.write(0, index_h, item_h)
+            for index_v, item_v in enumerate(values):
+                for index_col, item_col in enumerate(item_v):
+                    self.sheet.write(row + index_v, col + index_col, item_col, style)
+                    self.book.save(self.file_name)
